37d6a51d0b65e4c6adb6065b39858d223c88c3d2
Hien Tu <hientuminh@gmail.com>
2023-03-04 07:11:24 +0700

    Change secret key

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
-const SECRET =
-  '6D3DD80B6B40927146F39A2FB5A273C8A7C49C0856B8D4936CC34A90C7649E0B'
+const SECRET = process.env.SECRET
 
 const generateAccountToken = (accountInfo) => {
   try {

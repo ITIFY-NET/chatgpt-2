95294ba61eb0468dbfe916031165ee7ba2d8a129
dochet98 <mac@an-mac-pro.local>
2023-03-09 11:41:28 +0700

    update wirte

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,10 +1,7 @@
 // @ts-nocheck
-import {
-  ACCEPT_ERROR_STATUS,
-  BAD_REQUEST_CODE
-} from '../constants/responseCode'
+import { NOT_FOUND_CODE } from '../constants/responseCode'
 import { chatCompletionGeneration } from '../services/openai.service'
-import { handleMessageResponse } from '../utils/handle-response'
+import handleResponse from '../utils/handle-response'
 
 /**
  * Builds chat with user
@@ -17,39 +14,36 @@ export const create = async (request, response, next) => {
   const currentUser = request.currentUser
   try {
     const arrayBuffer = await chatCompletionGeneration(question, currentUser)
+    if (!arrayBuffer.status) {
+      return handleResponse({ message: arrayBuffer.response.statusText, code: arrayBuffer.response.status }, null, request, response)
+    }
     const contentType = arrayBuffer.headers['content-type']
     response.setHeader('content-type', contentType)
     let finalResult = ''
     // arrayBuffer.data.pipe(response)
     arrayBuffer.data.on('data', (data) => {
-      const objectStream = data.toString('utf-8').replace('data: ','')
+      const objectStream = data.toString('utf-8').replace('data: ', '')
       if (objectStream === `[DONE]\n\n` || objectStream === `[DONE]`) {
-        const dataResponse = `data: ${JSON.stringify({'result': null, 'status': 'DONE'})}`
+        const dataResponse = `data: ${JSON.stringify({ result: null, status: 'DONE' })}`
         response.write(dataResponse)
         response.end()
       } else {
         const objectJson = JSON.parse(objectStream)
         finalResult += objectJson.choices[0].text
-        const dataResponse = `data: ${JSON.stringify({'result': objectJson.choices[0].text, 'status': null})}\n\n`
+        const dataResponse = `data: ${JSON.stringify({ result: objectJson.choices[0].text, status: null })}\n\n`
         response.write(dataResponse)
       }
     })
     arrayBuffer.data.on('end', () => {
       if (!response.finished) {
-        const dataResponse = `data: ${JSON.stringify({'result': null, 'status': 'DONE'})}`
+        const dataResponse = `data: ${JSON.stringify({ result: null, status: 'DONE' })}`
         response.write(dataResponse)
         response.end()
       }
       // TODO: save history
-      console.log("All the result: ", finalResult)
+      console.log('All the result: ', finalResult)
     })
   } catch (error) {
-    console.log(error)
-    return handleMessageResponse(
-      error,
-      request,
-      response,
-      ACCEPT_ERROR_STATUS.includes(error.code) ? error.code : BAD_REQUEST_CODE
-    )
+    return handleResponse({ message: 'Service not found', code: NOT_FOUND_CODE }, null, request, response)
   }
 }
--- src/controllers/write.controller.js
+++ src/controllers/write.controller.js
@@ -3,6 +3,7 @@ import { textCompletionGeneration } from '../services/openai.service'
 import to from '../utils/to'
 import handleResponse from '../utils/handle-response'
 import { getModelSetting, getMasterCollection } from '../services/master'
+import { NOT_FOUND_CODE, SUCCESS_CODE } from '../constants/responseCode'
 /**
  * Builds chat with user
  * @param {any} req
@@ -10,35 +11,42 @@ import { getModelSetting, getMasterCollection } from '../services/master'
  * @param {any} next
  */
 export const create = async (req, res, next) => {
-  const { question, contextId } = req.body
-  const currentUser = req.currentUser
-  const arrayBuffer = await textCompletionGeneration(question, contextId, currentUser)
-  const contentType = arrayBuffer.headers['content-type']
-  res.setHeader('content-type', contentType)
-  // arrayBuffer.data.pipe(res)
-  let finalResult = ''
-  arrayBuffer.data.on('data', (data) => {
-    const streamData = data.toString('utf-8').replace('data: ','')
-    if (streamData === `[DONE]\n\n` || streamData === `[DONE]`) {
-      const dataResponse = `data: ${JSON.stringify({'result': null, 'status': 'DONE'})}`
-      res.write(dataResponse)
-      res.end()
-    } else {
-      const objectJson = JSON.parse(streamData)
-      finalResult += objectJson.choices[0].text
-      const dataResponse = `data: ${JSON.stringify({'result': objectJson.choices[0].text, 'status': null})}\n\n`
-      res.write(dataResponse)
+  try {
+    const { question, contextId } = req.body
+    const currentUser = req.currentUser
+    const arrayBuffer = await textCompletionGeneration(question, contextId, currentUser)
+    if (!arrayBuffer.status) {
+      return handleResponse({ message: arrayBuffer.response.statusText, code: arrayBuffer.response.status }, null, req, res)
     }
-  })
-  arrayBuffer.data.on('end', () => {
-    if (!res.finished) {
-      const dataResponse = `data: ${JSON.stringify({'result': null, 'status': 'DONE'})}`
-      res.write(dataResponse)
-      res.end()
-    }
-    // TODO: save history
-    console.log("All the result: ", finalResult)
-  })
+    const contentType = arrayBuffer.headers['content-type']
+    res.setHeader('content-type', contentType)
+    // arrayBuffer.data.pipe(res)
+    let finalResult = ''
+    arrayBuffer.data.on('data', (data) => {
+      const streamData = data.toString('utf-8').replace('data: ', '')
+      if (streamData === `[DONE]\n\n` || streamData === `[DONE]`) {
+        const dataResponse = `data: ${JSON.stringify({ result: null, status: 'DONE' })}`
+        res.write(dataResponse)
+        res.end()
+      } else {
+        const objectJson = JSON.parse(streamData)
+        finalResult += objectJson.choices[0].text
+        const dataResponse = `data: ${JSON.stringify({ result: objectJson.choices[0].text, status: null })}\n\n`
+        res.write(dataResponse)
+      }
+    })
+    arrayBuffer.data.on('end', () => {
+      if (!res.finished) {
+        const dataResponse = `data: ${JSON.stringify({ result: null, status: 'DONE' })}`
+        res.write(dataResponse)
+        res.end()
+      }
+      // TODO: save history
+      console.log('All the result: ', finalResult)
+    })
+  } catch (error) {
+    return handleResponse({ message: 'NOT FOUND SERVICE', code: NOT_FOUND_CODE }, null, req, res)
+  }
 }
 
 export const getModels = async (req, res, next) => {

d8b4daabe213cd982ccc13205a3656c59a843937
dochet98 <mac@an-mac-pro.local>
2023-03-13 10:21:40 +0700

    group collection

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -11,14 +11,39 @@ function emptyOrRows(rows) {
 
 function groupByKey(ungroupedArray, key) {
   return ungroupedArray.reduce(function (groupedArray, object) {
-      groupedArray[object[key]] = groupedArray[object[key]] || [];
-      groupedArray[object[key]].push(object)
-      return groupedArray
+    groupedArray[object[key]] = groupedArray[object[key]] || []
+    groupedArray[object[key]].push(object)
+    return groupedArray
   }, Object.create(null))
+}
+
+/**
+ * Builds chat with user
+ * @param {Array} data
+ * @param {string} key
+ */
+function groupCollection(data, key) {
+  const objGroup = {}
+  data.map((item) => {
+    const keyCheck = item[key]
+    if (!objGroup[keyCheck]) {
+      objGroup[keyCheck] = {
+        category: keyCheck,
+        data: [item]
+      }
+    } else {
+      objGroup[keyCheck]['data'].push(item)
+    }
+    return item
+  })
+  return Object.keys(objGroup).map((k) => {
+    return objGroup[k]
+  })
 }
 
 module.exports = {
   getOffset,
   emptyOrRows,
-  groupByKey
+  groupByKey,
+  groupCollection
 }
